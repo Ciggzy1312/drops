741cd58934382254e7326d2a4a2e5285cc12f64d
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2022-10-23 04:38:33 +0530

    Added cookie parser to handle authentication token
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 const dotenv = require('dotenv').config();
@@ -12,6 +13,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/api/auth', require('./routes/auth/authRoute'));
 
