5f0b999052cf1b610eacfa5e22b26ff8119fd2ad
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2022-10-22 20:21:57 +0530

    Added user model
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userbame: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    drops: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Drop',
+    }],
+    bookmarks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Drop',
+    }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
