ec76f928bd390bdbf1192e99c5428742c2b5ee19
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2022-10-23 07:50:03 +0530

    Added create drop function
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

--- server/controllers/drop/dropController.js
+++ server/controllers/drop/dropController.js
@@ -1,7 +1,21 @@
-
+const Drop = require('../../models/drop');
 
 const createDrop = async (req, res) => {
-    res.status(200).json({ message: "Create drop" });
+    const { name, description, tags } = req.body;
+
+    if(!name || !description || !tags) {
+        return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    const drop = await Drop.create({
+        name, description, tags
+    });
+
+    if(drop) {
+        return res.status(201).json({ message: "Drop created", drop });
+    } else {
+        return res.status(400).json({ message: "Invalid drop data" });
+    }
 }
 
 const getDrops = async (req, res) => {
--- server/routes/drop/dropRoute.js
+++ server/routes/drop/dropRoute.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { getDrop, getDrops, createDrop } = require('../../controllers/drop/dropController');
+const protect = require('../../middlewares/auth/authMiddleware');
 
 router.get('/', getDrops);
 
 router.get('/:id', getDrop);
 
-router.post('/', createDrop);
+router.post('/', protect, createDrop);
 
 module.exports = router;
