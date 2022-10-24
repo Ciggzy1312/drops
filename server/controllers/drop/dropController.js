c49815b64fe2046463b833e3d7b2a100b5fff335
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2022-10-24 05:40:24 +0530

    Added update and delete drop function
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

--- server/controllers/drop/dropController.js
+++ server/controllers/drop/dropController.js
@@ -39,4 +39,45 @@ const getDrop = async (req, res) => {
     }
 }
 
-module.exports = { createDrop, getDrops, getDrop };
+const updateDrop = async (req, res) => {
+    const { name, description, tags } = req.body;
+
+    if(!name || !description || !tags) {
+        return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    const drop = await Drop.findById(req.params.id);
+
+    if(drop && (drop.author.toString() !== req.user.id)){
+        return res.status(401).json({ message: "User not authorized to update this drop" });
+    };
+
+    const updatedDrop = await Drop.findByIdAndUpdate(req.params.id, {
+        name, description, tags
+    }, { new: true });
+
+    if(drop) {
+        return res.status(200).json({ message: "Drop updated", updatedDrop });
+    } else {
+        return res.status(400).json({ message: "Invalid drop data" });
+    }
+}
+
+const deleteDrop = async (req, res) => {
+
+    const drop = await Drop.findById(req.params.id);
+
+    if (drop && (drop.author.toString() !== req.user.id)) {
+        return res.status(401).json({ message: "User not authorized to delete this drop" });
+    };
+
+    const deletedDrop = await Drop.findByIdAndDelete(req.params.id);
+
+    if(drop) {
+        return res.status(200).json({ message: "Drop deleted", deletedDrop });
+    } else {
+        return res.status(400).json({ message: "Invalid drop data" });
+    }
+}
+
+module.exports = { createDrop, getDrops, getDrop, updateDrop, deleteDrop };
--- server/routes/drop/dropRoute.js
+++ server/routes/drop/dropRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getDrop, getDrops, createDrop } = require('../../controllers/drop/dropController');
+const { getDrop, getDrops, createDrop, updateDrop, deleteDrop } = require('../../controllers/drop/dropController');
 const protect = require('../../middlewares/auth/authMiddleware');
 
 router.get('/', protect, getDrops);
@@ -9,5 +9,9 @@ router.get('/', protect, getDrops);
 router.get('/:id', protect, getDrop);
 
 router.post('/', protect, createDrop);
+
+router.patch('/:id', protect, updateDrop);
+
+router.delete('/:id', protect, deleteDrop);
 
 module.exports = router;
