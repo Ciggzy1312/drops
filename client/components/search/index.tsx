c086bc0a4a84f47bcd9d11ae0e0ee038c055ab34
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2022-11-18 21:41:49 +0530

    Added create drop feature
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

--- client/components/search/index.tsx
+++ client/components/search/index.tsx
@@ -4,7 +4,7 @@ import { FC } from "react"
 const SearchBar: FC = () => {
 
     return (
-        <div className="">
+        <div className="flex-grow">
             <input className="px-2 py-1 border border-gray-400 w-1/4 rounded-l-md focus:outline-none" type="text" placeholder="Search for drops" />
 
             <button className="text-white px-4 py-1 border border-black bg-black rounded-r-md">Search</button>
--- client/pages/dashboard/index.tsx
+++ client/pages/dashboard/index.tsx
@@ -5,10 +5,12 @@ import DropCard from "../../components/drop/dropCard";
 import { DataType, DropType } from "../../types/types";
 import jwt_decode from "jwt-decode";
 import SearchBar from "../../components/search";
+import CreateDrop from "../../components/form/createDrop";
 
 const Dashboard: NextPage<{ data: DataType, token: string }> = ({data, token}) => {
 
     const [dropsState, setDropsState] = useState<DropType[]>(data.drops);
+    const [isOpen, setIsOpen] = useState(false);
 
     const getData = async () => {
         const val = await axios.get(`${process.env.NEXT_PUBLIC_URL}api/drop`, { withCredentials: true });
@@ -23,8 +25,14 @@ const Dashboard: NextPage<{ data: DataType, token: string }> = ({data, token}) =
         <div className="px-6">
             <h1>Dashboard</h1>
 
-            <div className="my-4">
+            <div className="my-4 flex">
                 <SearchBar />
+            </div>
+
+            <div className="my-4 flex justify-between">
+                <div className="text-2xl font-semibold">All resources</div>
+                <button className="bg-indigo-500 rounded-md px-5 py-1 font-medium text-white" onClick={() => setIsOpen(!isOpen)}>Create Drop</button>
+                {isOpen && <CreateDrop isOpen={isOpen} setIsOpen={setIsOpen} />}
             </div>
 
             <div className="grid grid-cols-4 gap-4">
