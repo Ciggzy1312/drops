70714dde389241fa7f55f3943af93a2cef8c2dcc
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2022-12-03 18:47:57 +0530

    UI improvement 5.0 : Login screen
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

--- client/components/auth/login.tsx
+++ client/components/auth/login.tsx
@@ -27,30 +27,30 @@ const LoginForm: FC = () => {
             <div className="w-full">
 
                 <div className="my-2">
-                    <h2 className="mt-6 text-3xl font-bold text-gray-700">Welcome back</h2>
-                    <p className="mt-1 text-sm font-medium text-gray-400">Please enter your details</p>
+                    <h2 className="mt-6 text-3xl font-bold text-white">Welcome back</h2>
+                    <p className="mt-1 text-sm font-medium text-[#A0A6B1]">Please enter your details</p>
                 </div>
 
                 <div className="">
                     <form className="" onSubmit={handleSubmit}>
-                        <div className="font-medium text-neutral-500">
+                        <div className="font-medium text-white">
                             <div className="flex flex-col">
                                 <label className="py-2 text-sm">Email</label>
-                                <input type="email" required value={email} className="px-3 py-2 border border-gray-300 text-gray-700 rounded-md focus:outline-none focus:border-indigo-400 sm:text-sm" onChange={(e) => setEmail(e.target.value)} />
+                                <input type="email" required value={email} className="px-3 py-2 bg-[#252728] border border-[#252728] text-[#BCC0C8] rounded-md focus:outline-none focus:border-[#3F8DFD] sm:text-sm" onChange={(e) => setEmail(e.target.value)} />
                             </div>
 
                             <div className="flex flex-col">
                                 <label className="py-2 text-sm">Password</label>
-                                <input type="password" required value={password} className="px-3 py-2 border border-gray-300 text-gray-700 rounded-md focus:outline-none focus:border-indigo-400 sm:text-sm" onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" required value={password} className="px-3 py-2 bg-[#252728] border border-[#252728] text-[#BCC0C8] rounded-md focus:outline-none focus:border-[#3F8DFD] sm:text-sm" onChange={(e) => setPassword(e.target.value)} />
                             </div>
                         </div>
 
                         <div className="mt-8">
-                            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign In</button>
+                            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#3F8DFD] hover:bg-[#1877FF] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:bg-[#1877FF]">Sign In</button>
                         </div>
 
-                        <div className="mt-5 text-sm text-gray-500 text-center">
-                            <h3 className="">Don&apos;t have an account? <Link href={"/auth/register"}><span className="text-indigo-400 font-medium cursor-pointer">Sign Up</span></Link></h3>
+                        <div className="mt-5 text-sm text-[#A0A6B1] text-center">
+                            <h3 className="">Don&apos;t have an account? <Link href={"/auth/register"}><span className="text-[#73ACFF] hover:text-[#3F8DFD] font-medium cursor-pointer">Sign Up</span></Link></h3>
                         </div>
                     </form>
                 </div>
